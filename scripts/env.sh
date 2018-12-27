ad730de40a89d3d5e578f371ad1b5c6da82deeea
Slathia <slathiasachin13@gmail.com>
2018-12-27 16:32:20 +0530

    Added scripts folder in project

---
+++ scripts/env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export GIT_UR='https://github.com/slathia15/mavenProject'
+export CREDENTIAL_ID='13630e07-031f-4ac8-8a06-ca168d6d1e7f'
+export GIT_DEVOPS_URL='https://github.com/slathia15/generic-application-Devops'
