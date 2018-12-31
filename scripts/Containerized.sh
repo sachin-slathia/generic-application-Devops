9a006e1109d09fc20b077f012d38952f8f6ac9f2
Slathia <slathiasachin13@gmail.com>
2018-12-31 10:35:14 +0530

    Added Containerized.sh

--- scripts/Containerized.sh
+++ scripts/Containerized.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 git_commit=env.BRANCH_NAME
 echo "${env.BRANCH_NAME}"
-echo "$docker_login_password"
+echo "docker login password="
+echo "${docker_login_password}"
 docker login --username=slathia15 --password=\'$docker_login_password\' hub.docker.com
 #tag =play:{$git_commit}
 #docker build -t tag .
