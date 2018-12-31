2c2b8ddfbaa006c654dd8100f0eec7745889024f
Slathia <slathiasachin13@gmail.com>
2018-12-31 12:59:49 +0530

    Added changes in file

--- groovy/build_docker_services.groovy
+++ groovy/build_docker_services.groovy
@@ -3,6 +3,7 @@
 def build(String repo, String username, String password, String branch4tagging) {
   gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
   shortCommit = gitCommit.take(7)
+  echo "$password"
   tag = "$branch4tagging-$shortCommit"
   sh """
       set -v
