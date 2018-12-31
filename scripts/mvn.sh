c74efea8ca39867235a27e94cbc54824e53bc84d
Slathia <slathiasachin13@gmail.com>
2018-12-31 16:24:29 +0530

    Added mvn.sh in scripts.sh

--- scripts/mvn.sh
+++ scripts/mvn.sh
@@ -4,6 +4,7 @@ COMPILE_ARGS="compile"
 TEST_ARGS="test"
 CHECK_STYLE="checkstyle:checkstyle"
 PMD_ARGS="pmd:pmd"
+PACKAGE_ARGS="package"
 
-mvn clean $COMPILE_ARGS $CHECK_STYLE $TEST_ARGS $PMD_ARGS
+mvn clean $COMPILE_ARGS $CHECK_STYLE $TEST_ARGS $PMD_ARGS $PACKAGE_ARGS
 
