73bd9c72eda9f21ba041fa00aef871a753b2cd73
slathia15 <slathiasachin13@gmail.com>
2019-01-03 10:12:20 +0530

    Added sudo in docker command

--- scripts/Containerized.sh
+++ scripts/Containerized.sh
@@ -3,6 +3,6 @@ git_commit=env.BRANCH_NAME
 echo "${env.BRANCH_NAME}"
 echo "docker login password="
 echo $docker_login_password
-docker login --username=slathia15 --password=\'$docker_login_password\' hub.docker.com
+sudo docker login --username=slathia15 --password=\'$docker_login_password\' hub.docker.com
 #tag =play:{$git_commit}
 #docker build -t tag .
