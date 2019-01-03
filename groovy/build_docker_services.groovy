6b0a5af6ad017ee06591a2e585e702af7e590101
slathia15 <slathiasachin13@gmail.com>
2019-01-03 12:50:37 +0530

    Added file in jenkins

--- groovy/build_docker_services.groovy
+++ groovy/build_docker_services.groovy
@@ -4,7 +4,9 @@ def build(String repo, String username, String password, String branch4tagging) 
   gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
   shortCommit = gitCommit.take(7)
   echo "$password"
-  tag = "$branch4tagging-$shortCommit"
+   pwd 
+   ls -la
+ tag = "$branch4tagging-$shortCommit"
   sh """
       echo $pwd
       set -v
