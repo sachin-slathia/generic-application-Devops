0112f565190209cb807dab3adb2c3203490ce4f6
slathia15 <slathiasachin13@gmail.com>
2019-01-04 14:56:57 +0530

    Added ansible folder

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ansible-playbook /home/sachin/Sachin/Sachin/data/Personal/Personal/DEVOPS/Ansible/final/unzip.yml -become -u ec2-user 
+ansible-playbook ./Devops/ansible/unzip.yml -become -u ec2-user 
